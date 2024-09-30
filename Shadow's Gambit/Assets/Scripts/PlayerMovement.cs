631035bd0c836bfb35041aa44d2e61c29fdd63fc
PensiveProgrammer64 <92233946+PensiveProgrammer64@users.noreply.github.com>
2024-09-30 09:11:39 -0400

    Added Climbing to Character Controller and Updated Crawling
    Changed Crawling to work as a part of Update() and not as its own separate method as that was less practical, also added Climbing for ropes and ladders and such by making contact with an object that has the tag "Climbable". Added object to climb.

--- Shadow's Gambit/Assets/Scripts/PlayerMovement.cs
+++ Shadow's Gambit/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,9 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     Vector2 movement = Vector2.zero;
     bool isCrawling = false;
-    bool playerOnLadder = false; // Currently unused, however we will add it when we have ladders.
+    bool isClimbing = false;
     [SerializeField] float moveSpeed = 200;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,15 +18,25 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal"); // This is A, D, LeftArrow, and RightArrow
-        movement.y = Input.GetAxisRaw("Vertical"); // This is W, S, UpArrow, and DownArrow
-        if(!playerOnLadder && Input.GetKeyDown(KeyCode.S)) // This is for Crawling
-        {
-            Debug.Log("Player is Crawling"); // Remove this when ready to submit
-            Crawl();
-        }
-    }
+        movement.x = Input.GetAxisRaw("Horizontal"); // A, D, LeftArrow, and RightArrow
+        movement.y = Input.GetAxisRaw("Vertical");   // W, S, UpArrow, and DownArrow
 
+        // Replace this with switch case
+        // Worth trying to change the `Input.GetKey(KeyCode.(LeftControl and W))` to `Input.GetAxisRaw("")` for control compatibility
+        if (!isClimbing && Input.GetKey(KeyCode.LeftControl)) // Start crawling
+        {
+            isCrawling = true;
+            moveSpeed = 100; // Change speed when crawling
+            Debug.Log("Player is Crawling"); // Remove this when ready to submit
+        }
+        else
+        {
+            isCrawling = false;
+            moveSpeed = 200; // Reset speed when not crawling
+            // Debug.Log("Player has Stopped Crawling"); // Remove this when ready to submit
+        }
+        // Need to change this to only work while in contact with object with tag "Climbable" or something to that effect        
+    }
     private void FixedUpdate()
     {
         if (movement.sqrMagnitude > 0)
@@ -33,12 +44,25 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = movement * moveSpeed * Time.deltaTime;
         }
     }
-
-    private void Crawl()
+    private void OnCollisionEnter2D(Collision2D collision) // This code doesn't work very well. It has issues recognizing contact with Climbables (It has to do with pressing W key to climb). It does not work if BoxCollider2D on object is set to "Is Trigger".
     {
-        // Change player animation / sprite
-        // Change player movement speed
-        // Change player height (For fitting in tunnels. Might be unnecessary)
-        // Lower detection multiplier
+        if (collision.gameObject.CompareTag("Climbable"))
+        {
+            if (!isCrawling && Input.GetKey(KeyCode.W)) // Start climbing
+            {
+                isClimbing = true;
+                rb.gravityScale = 0; // Disables gravity when climbing
+                Debug.Log("Player is Climbing"); // Remove this when ready to submit
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Climbable"))
+        {
+            isClimbing = false;
+            rb.gravityScale = 1; // Enables gravity when not climbing
+            Debug.Log("Player has Stopped Climbing"); // Remove this when ready to submit
+        }
     }
 }
